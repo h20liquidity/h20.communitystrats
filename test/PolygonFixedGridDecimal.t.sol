4df6dc1d9b3f21542d9c29dff60fe6386ab2121c
Siddharth2207 <siddharthbhoite22@gmail.com>
2024-06-28 22:29:56 +0530

    update strat

--- test/PolygonFixedGridDecimal.t.sol
+++ test/PolygonFixedGridDecimal.t.sol
@@ -61,7 +61,7 @@ contract PolygonFixedGridDecimal is StrategyTests {
     using SafeERC20 for IERC20;
     using Strings for address;
 
-    uint256 constant FORK_BLOCK_NUMBER = 58154703;
+    uint256 constant FORK_BLOCK_NUMBER = 58715054;
    
     
     function selectFork() internal {
@@ -89,80 +89,6 @@ contract PolygonFixedGridDecimal is StrategyTests {
         ORDER_OWNER = address(0x19f95a84aa1C48A2c6a7B2d5de164331c86D030C);
     }
 
-    function testBountyAuction() public {
-
-        IO[] memory inputVaults = new IO[](1);
-        inputVaults[0] = polygonUsdcIo();
-
-        IO[] memory outputVaults = new IO[](1);
-        outputVaults[0] = polygonLinkIo();
-
-        LibStrategyDeployment.StrategyDeployment memory strategy = LibStrategyDeployment.StrategyDeployment(
-            getEncodedBuyLinkRoute(address(ARB_INSTANCE)),
-            getEncodedSellLinkRoute(address(ARB_INSTANCE)),
-            0,
-            0,
-            1e6,
-            10000e18,
-            0,
-            0,
-            "strategies/polygon-link-fixed-grid.rain",
-            "polygon-link-fixed-grid.sell.deviation.prod",
-            "./lib/h20.test-std/lib/rain.orderbook",
-            "./lib/h20.test-std/lib/rain.orderbook/Cargo.toml",
-            inputVaults,
-            outputVaults
-        ); 
-
-        OrderV2 memory order = addOrderDepositOutputTokens(strategy);
-        {
-            vm.recordLogs();
-            takeArbOrder(order, strategy.takerRoute, strategy.inputTokenIndex, strategy.outputTokenIndex);
-            Vm.Log[] memory entries = vm.getRecordedLogs();
-            uint256 bounty = getBounty(entries);
-
-            // Assert greater than max bounty.
-            assertGe(bounty, 0.3e6);
-        }
-
-        // Cooldown
-        vm.warp(block.timestamp + 14400); 
-        {
-            vm.recordLogs();
-            takeArbOrder(order, strategy.takerRoute, strategy.inputTokenIndex, strategy.outputTokenIndex);
-            Vm.Log[] memory entries = vm.getRecordedLogs();
-            uint256 bounty = getBounty(entries);
-
-            // Assert greater than min-bounty + (bounty-units * time-since-cooldown)
-            // 0.015 + (0.01 * 0)
-            assertGe(bounty, 0.015e6);
-        }
-        // 60 seconds after cooldown
-        vm.warp(block.timestamp + 14400 + 60); 
-        {
-            vm.recordLogs();
-            takeArbOrder(order, strategy.takerRoute, strategy.inputTokenIndex, strategy.outputTokenIndex);
-            Vm.Log[] memory entries = vm.getRecordedLogs();
-            uint256 bounty = getBounty(entries);
-
-            // Assert greater than min-bounty + (bounty-units * time-since-cooldown)
-            // 0.015 + (0.01 * 1)
-            assertGe(bounty, 0.025e6);
-        }
-        // 120 seconds after cooldown
-        vm.warp(block.timestamp + 14400 + 120); 
-        {
-            vm.recordLogs();
-            takeArbOrder(order, strategy.takerRoute, strategy.inputTokenIndex, strategy.outputTokenIndex);
-            Vm.Log[] memory entries = vm.getRecordedLogs();
-            uint256 bounty = getBounty(entries);
-
-            // Assert greater than min-bounty + (bounty-units * time-since-cooldown)
-            // 0.015 + (0.01 * 2)
-            assertGe(bounty, 0.035e6);
-        }
-    }
-
     function testLinkBuyLinkHappyPath() public {
 
         IO[] memory inputVaults = new IO[](1);
@@ -171,8 +97,8 @@ contract PolygonFixedGridDecimal is StrategyTests {
         IO[] memory outputVaults = new IO[](1);
         outputVaults[0] = polygonUsdcIo();
 
-        uint256 expectedRatio = 64053597844087947;
-        uint256 expectedAmountOutputMax = 16279555611016865760;
+        uint256 expectedRatio = 69352715075810792;
+        uint256 expectedAmountOutputMax = 17098814843865977310;
 
         LibStrategyDeployment.StrategyDeployment memory strategy = LibStrategyDeployment.StrategyDeployment(
             getEncodedSellLinkRoute(address(ARB_INSTANCE)),
@@ -183,13 +109,14 @@ contract PolygonFixedGridDecimal is StrategyTests {
             10000e6,
             expectedRatio,
             expectedAmountOutputMax,
-            "strategies/polygon-link-fixed-grid.rain",
-            "polygon-link-fixed-grid.buy.deviation.prod",
+            "strategies/dca-oracle-polygon-univ3.rain",
+            "polygon-link-dca-univ3.buy.deviation.prod",
             "./lib/h20.test-std/lib/rain.orderbook",
             "./lib/h20.test-std/lib/rain.orderbook/Cargo.toml",
             inputVaults,
             outputVaults
         );
+
 
         // ArbOrderTaker 'arb'
         checkStrategyCalculationsArbOrder(strategy);
@@ -204,8 +131,8 @@ contract PolygonFixedGridDecimal is StrategyTests {
         IO[] memory outputVaults = new IO[](1);
         outputVaults[0] = polygonLinkIo();
 
-        uint256 expectedRatio = 15077777712463462481;
-        uint256 expectedAmountOutputMax = 1059808409153557385;
+        uint256 expectedRatio = 13949412411209658325; 
+        uint256 expectedAmountOutputMax = 1139920989591065154;
 
         LibStrategyDeployment.StrategyDeployment memory strategy = LibStrategyDeployment.StrategyDeployment(
             getEncodedBuyLinkRoute(address(ARB_INSTANCE)),
@@ -216,42 +143,18 @@ contract PolygonFixedGridDecimal is StrategyTests {
             10000e18,
             expectedRatio,
             expectedAmountOutputMax,
-            "strategies/polygon-link-fixed-grid.rain",
-            "polygon-link-fixed-grid.sell.deviation.prod",
+            "strategies/dca-oracle-polygon-univ3.rain",
+            "polygon-link-dca-univ3.sell.deviation.prod",
             "./lib/h20.test-std/lib/rain.orderbook",
             "./lib/h20.test-std/lib/rain.orderbook/Cargo.toml",
             inputVaults,
             outputVaults
         );
 
-        {
-            (bytes memory bytecode, uint256[] memory constants) = PARSER.parse(
-                LibComposeOrders.getComposedOrder(
-                    vm, strategy.strategyFile, strategy.strategyScenario, strategy.buildPath, strategy.manifestPath
-                )
-            );
-            (,,address expression,) = EXPRESSION_DEPLOYER.deployExpression2(bytecode, constants); 
-            uint256[][] memory context = getOrderContext(uint256(keccak256("order-hash")));
-
-            // Eval Order
-            (uint256[] memory stack,) = INTERPRETER.eval2(
-                STORE,
-                getNamespace(),
-                LibEncodedDispatch.encode2(expression, SourceIndexV2.wrap(0), type(uint16).max),
-                context,
-                new uint256[](0)
-            );
-
-            for(uint256  i = 0 ; i < stack.length; i++){
-                console2.log("stack[%s] : %s",i, stack[i]);
-            }
-
-        }
-
-
         // ArbOrderTaker 'arb'
-        // checkStrategyCalculationsArbOrder(strategy);
-    } 
+        checkStrategyCalculationsArbOrder(strategy);
+
+    }
 
     function testGridCooldown() public {
 
@@ -270,8 +173,8 @@ contract PolygonFixedGridDecimal is StrategyTests {
             10000e6,
             0,
             0,
-            "strategies/polygon-link-fixed-grid.rain",
-            "polygon-link-fixed-grid.buy.deviation.prod",
+            "strategies/dca-oracle-polygon-univ3.rain",
+            "polygon-link-dca-univ3.buy.deviation.prod",
             "./lib/h20.test-std/lib/rain.orderbook",
             "./lib/h20.test-std/lib/rain.orderbook/Cargo.toml",
             inputVaults,
@@ -300,23 +203,111 @@ contract PolygonFixedGridDecimal is StrategyTests {
 
 
     }
+    
+    function testPartialTrade() public {
+
+        IO[] memory inputVaults = new IO[](1);
+        inputVaults[0] = polygonUsdcIo();
+
+        IO[] memory outputVaults = new IO[](1);
+        outputVaults[0] = polygonLinkIo();
+
+        uint256 expectedRatio = 14018452504312447172;
+        uint256 expectedAmountOutputMax = 1085303707401124384;
+
+        LibStrategyDeployment.StrategyDeployment memory strategy = LibStrategyDeployment.StrategyDeployment(
+            getEncodedBuyLinkRoute(address(ARB_INSTANCE)),
+            getEncodedSellLinkRoute(address(ARB_INSTANCE)),
+            0,
+            0,
+            1e6,
+            10000e18,
+            expectedRatio,
+            expectedAmountOutputMax,
+            "strategies/dca-oracle-polygon-univ3.rain",
+            "polygon-link-dca-univ3.sell.deviation.prod",
+            "./lib/h20.test-std/lib/rain.orderbook",
+            "./lib/h20.test-std/lib/rain.orderbook/Cargo.toml",
+            inputVaults,
+            outputVaults
+        );
+
+        OrderV2 memory order = addOrderDepositOutputTokens(strategy);
+
+        // Get the order output max for the order
+        uint256 orderOuputMax;
+        {
+            (bytes memory bytecode, uint256[] memory constants) = PARSER.parse(
+                LibComposeOrders.getComposedOrder(
+                    vm, strategy.strategyFile, strategy.strategyScenario, strategy.buildPath, strategy.manifestPath
+                )
+            );
+            (,,address expression,) = EXPRESSION_DEPLOYER.deployExpression2(bytecode, constants); 
+            uint256[][] memory context = getOrderContext(uint256(keccak256("order-hash")));
+
+            // Eval Order
+            (uint256[] memory stack,) = INTERPRETER.eval2(
+                STORE,
+                getNamespace(),
+                LibEncodedDispatch.encode2(expression, SourceIndexV2.wrap(0), type(uint16).max),
+                context,
+                new uint256[](0)
+            );
+            orderOuputMax = stack[1];
+        }
+
+        vm.startPrank(APPROVED_EOA);
+        address inputTokenAddress = order.validInputs[strategy.inputTokenIndex].token;
+        IERC20(inputTokenAddress).safeApprove(address(ORDERBOOK), type(uint256).max);
+        TakeOrderConfigV2[] memory innerConfigs = new TakeOrderConfigV2[](1);
+        innerConfigs[0] = TakeOrderConfigV2(order, strategy.inputTokenIndex, strategy.outputTokenIndex, new SignedContextV1[](0));
+
+        // If taker order amount is less than order output max, the trade fails
+        {   
+            TakeOrdersConfigV2 memory takeOrdersConfig =
+            TakeOrdersConfigV2(0, 1, type(uint256).max, innerConfigs, "");
+            vm.expectRevert("Partial trade");
+            ORDERBOOK.takeOrders(takeOrdersConfig);
+        }
+        {   
+            TakeOrdersConfigV2 memory takeOrdersConfig =
+            TakeOrdersConfigV2(0, orderOuputMax - 1 , type(uint256).max, innerConfigs, "");
+            vm.expectRevert("Partial trade");
+            ORDERBOOK.takeOrders(takeOrdersConfig);
+        }
+        // Else trade succeeds
+        {   
+            TakeOrdersConfigV2 memory takeOrdersConfig =
+            TakeOrdersConfigV2(0, orderOuputMax, type(uint256).max, innerConfigs, "");
+            ORDERBOOK.takeOrders(takeOrdersConfig);
+        }
+        vm.stopPrank();
+
+    }
 
     function getBounty(Vm.Log[] memory entries)
         public
         view
-        returns (uint256 bounty)
-    {
+        returns (uint256 inputTokenBounty, uint256 outputTokenBounty)
+    {   
+        // Array of length 2 to store the input and ouput token bounties.
+        uint256[] memory bounties = new uint256[](2);
+
+        // Count the number of bounties found.
+        uint256 bountyCount = 0;
         for (uint256 j = 0; j < entries.length; j++) { 
             if (
                 entries[j].topics[0] == keccak256("Transfer(address,address,uint256)") && 
                 address(ARB_INSTANCE) == abi.decode(abi.encodePacked(entries[j].topics[1]), (address)) &&
                 address(APPROVED_EOA) == abi.decode(abi.encodePacked(entries[j].topics[2]), (address))
-                ) {
-
-                bounty = abi.decode(entries[j].data, (uint256));
+            ) {
+                bounties[bountyCount] = abi.decode(entries[j].data, (uint256));
+                bountyCount++;
             }   
         }
-    }
+        return (bounties[0], bounties[1]);
+    } 
+
 
     function getEncodedBuyLinkRoute(address toAddress) internal pure returns (bytes memory) {
         bytes memory ROUTE_PRELUDE =
@@ -327,9 +318,9 @@ contract PolygonFixedGridDecimal is StrategyTests {
 
     function getEncodedSellLinkRoute(address toAddress) internal pure returns (bytes memory) {
         bytes memory ROUTE_PRELUDE =
-            hex"0253E0bca35eC356BD5ddDFebbD1Fc0fD03FaBad3901ffff0122177148e681A6ca5242c9888Ace170EE7eC47BD00";
+            hex"0253E0bca35eC356BD5ddDFebbD1Fc0fD03FaBad3901ffff013c92bd9596E6aca8cfa29ec733ebe0c189bec1F301E7eb31f23A5BefEEFf76dbD2ED6AdC822568a5d201c2132D05D31c914a87C6611C10748AEb04B58e8F01ffff018CFaab34f5159abf9C35587AC40d09a05dC9476500";
             
         return abi.encode(bytes.concat(ROUTE_PRELUDE, abi.encodePacked(address(toAddress))));
     }
 
-}//0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174::transfer(0x669845c29D9B1A64FFF66a55aA13EB4adB889a88
+}
